eff244ca71daa20427f5559b126da8af0d97dccd
ALIM <yanis.alim@etu.univ-paris-diderot.fr>
2019-04-18 03:31:16 +0200

    this header works fine for send & recv TLVs

--- src/header.c
+++ src/header.c
@@ -6,8 +6,9 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-##include "header.h"
-
+#include <inttypes.h>
+#include <time.h>
+#include "header.h"
 
 char* mac_eth0()
 {
@@ -23,36 +24,119 @@ char* mac_eth0()
      return mac;
 }
 
-
-uint64_t init_peer()
+uint64_t generate_id()
 {
-     unsigned char temp[SHA_DIGEST_LENGTH];
-     char buf[SHA_DIGEST_LENGTH*2];
+     uint64_t hash = 17570031337;
+     int c;
 
-     memset(buf, 0x0, SHA_DIGEST_LENGTH*2);
-     memset(temp, 0x0, SHA_DIGEST_LENGTH);
+     char *mac = mac_eth0();
 
-     char* mac = mac_eth0();
-     SHA1((unsigned char *) mac, strlen(mac), temp);
+     while ((c = *mac++))
+          hash = ((hash << 5) + hash) + c;
 
-     for (int i=0; i < SHA_DIGEST_LENGTH; i++)
-         sprintf((char*)&(buf[i*2]), "%02x", temp[i]);
-
-     return (uint64_t) buf;
+     return hash;
 }
 
-int check_header(msg_hd header)
+uint32_t generate_nonce()
 {
-     return ( header.magic == 93 && header.version == 2 );
+     time_t s = time(NULL);
+     return (uint32_t) s;
 }
 
-int msg_type(msg_packet m)
+int check_header(msg_packet* m)
 {
-     if ( ! check_header(m.header) )
-          return -1;
+     return ( m->header.magic == 93 && m->header.version == 2 );
+}
 
-     else if ( m.tlv.type > 7 )
-          return -1;
-     else
-          return m.tlv.type
+void create_header(msg_packet* m, uint16_t length)
+{
+     m->header.magic = 93;
+     m->header.version = 2;
+     m->header.body_length = length;
+}
+
+void create_pad1(msg_packet* m)
+{
+     uint16_t body_length = 1;
+     create_header(m,body_length);
+     m->tlv.pad1.type = 0;
+}
+
+void create_padN(msg_packet* m, uint8_t length)
+{
+     uint16_t body_length = length+2;
+     create_header(m,body_length);
+     m->tlv.padn.type = 1;
+     m->tlv.padn.length = length;
+     memset(m->tlv.padn.MBZ,0x00,length);
+}
+
+void create_short_hello(msg_packet* m)
+{
+     uint16_t body_length = 1+1+8;
+     create_header(m,body_length);
+     m->tlv.s_hello.type = 2;
+     m->tlv.s_hello.length = 0x08;
+     m->tlv.s_hello.src_id = MY_ID;
+}
+
+void create_long_hello(msg_packet* m, uint64_t dst_id)
+{
+     uint16_t body_length = 1+1+16;
+     create_header(m,body_length);
+     m->tlv.l_hello.type = 2;
+     m->tlv.l_hello.length = 0x10;
+     m->tlv.l_hello.src_id = MY_ID;
+     m->tlv.l_hello.dst_id = dst_id;
+}
+
+void create_neighbour(msg_packet* m, peer* p)
+{
+     uint16_t body_length = 1+1+16+2;
+     create_header(m,body_length);
+     m->tlv.neighbour.type = 3;
+     m->tlv.neighbour.length = 0x12;
+     memcpy(m->tlv.neighbour.ip,p->ip,16);
+     m->tlv.neighbour.port = p->port;
+}
+
+void create_data(msg_packet* m, uint8_t length, uint64_t sender_id, uint32_t nonce, uint8_t type, char* msg)
+{
+     uint16_t body_length = 1+1+length;
+     create_header(m,body_length);
+     m->tlv.data.type = 4;
+     m->tlv.data.length = length;
+     m->tlv.data.sender_id = sender_id;
+     m->tlv.data.nonce = nonce;
+     m->tlv.data.type = type;
+     memcpy(m->tlv.data.message, msg, length-13);
+}
+
+void create_ack(msg_packet* m, uint32_t nonce)
+{
+     uint16_t body_length = 1+1+8+4;
+     create_header(m,body_length);
+     m->tlv.ack.type = 5;
+     m->tlv.ack.length = 12;
+     m->tlv.ack.sender_id = MY_ID;
+     m->tlv.ack.nonce = nonce;
+}
+
+void create_goaway(msg_packet* m, uint8_t length, uint8_t code, char* msg)
+{
+     uint16_t body_length = 1+1+length;
+     create_header(m,body_length);
+     m->tlv.goaway.type = 6;
+     m->tlv.goaway.length = length;
+     m->tlv.goaway.code = code;
+     memcpy(m->tlv.goaway.message, msg, length-1);
+}
+
+void create_warning(msg_packet* m, uint8_t length, char* msg)
+{
+     uint16_t body_length = 1+1+length;
+     create_header(m,body_length);
+     m->tlv.warning.type = 6;
+     m->tlv.warning.length = length;
+     memcpy(m->tlv.warning.message, msg, length);
 }
--- src/header.h
+++ src/header.h
@@ -1,9 +1,13 @@
+#ifndef HEADER_H_INCLUDED
+#define HEDAER_H_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <time.h>
 
 #define MSG_SIZE 4096-8    // UDP Header is 8 bytes
+
+uint64_t MY_ID;
 
 typedef uint8_t type_t;
 enum type_enum
@@ -25,73 +29,96 @@ typedef struct
      uint8_t magic;           // (magic != 93)  -> ignore
      uint8_t version;         // (version != 2) -> ignore
      uint16_t body_length;    // (body_length > PACKET_SIZE-4) -> ignore out of bounds data
-} msg_hd;
+} __attribute__((packed, scalar_storage_order("big-endian"))) msg_hd;
+
+
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv0;
 
 typedef struct
 {
      type_t type;             // if (type != [0-7]) -> ignore
      uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
-     union
-     {
-          struct
-          {
-               char trash[4082];
-          } pad1;
+     char MBZ[MSG_SIZE-6];
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv1;
 
-          struct
-          {
-               char MBZ[4082];
-          } padn;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     uint64_t src_id;                                            // the hash of the mac-address
 
-          struct
-          {
-               uint64_t src_id;              // the hash of the mac-address
-          } hello_short ;
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv21;
 
-          struct
-          {
-               uint64_t src_id;
-               uint64_t dest_id;
-          } hello_long;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     uint64_t src_id;
+     uint64_t dst_id;
 
-          struct
-          {
-               unsigned char ip[16];
-               uint16_t port;
-          } neighbour;
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv22;
 
-          struct
-          {
-               uint64_t sender_id;
-               unit32_t nonce;          // encode actual time
-               unit8_t type;            // (type == 0) ? print : forward
-               char message[4069];
-          } data;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     unsigned char ip[16];
+     uint16_t port;
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv3;
 
-          struct
-          {
-               uint64_t sender_id;
-               unit32_t nonce;
-          } ack;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     uint64_t sender_id;
+     uint32_t nonce;          // encode actual time
+     uint8_t data_type;            // (type == 0) ? print : forward
+     char message[MSG_SIZE-6-13];
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv4;
 
-          struct
-          {
-               uint8_t code;                // [0-3]
-               char messgae[4081];
-          } goaway;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     uint64_t sender_id;
+     uint32_t nonce;
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv5;
 
-          struct
-          {
-               char message[4082];
-          } warning;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     uint8_t code;                           // [0-3]
+     char message[MSG_SIZE-6-1];
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv6;
 
-    } value;
-} msg_body;
+typedef struct
+{
+     type_t type;             // if (type != [0-7]) -> ignore
+     uint8_t length;          // if (length > body_length-2) -> ignore out of bounds
+     char message[MSG_SIZE-6];
+} __attribute__((packed, scalar_storage_order("big-endian"))) tlv7;
+
+union msg_body
+{
+     tlv0  pad1;
+     tlv1  padn;
+     tlv21 s_hello;
+     tlv22 l_hello;
+     tlv3  neighbour;
+     tlv4  data;
+     tlv5  ack;
+     tlv6  goaway;
+     tlv7  warning;
+};
 
 typedef struct
 {
      msg_hd header;
-     msg_body tlv;
+     union msg_body tlv;
 } msg_packet;
 
 
@@ -99,25 +126,51 @@ typedef struct
 {
      unsigned char ip[16];
      uint16_t port;
-} peer;
+}  __attribute__((packed, scalar_storage_order("big-endian"))) peer;
 
-typedef struct neighbour
+typedef struct neighbour_list
 {
      uint64_t id;
      peer*  ip_port;
      time_t hello_t;
      time_t long_hello_t;
-     struct neighbour* next;
-} neighbour;
+     int retries;
+     struct neighbour_list* next;
+}  __attribute__((packed, scalar_storage_order("big-endian"))) neighbour_list;
 
 typedef struct
 {
-     neighbour* symteric;
-     neighbour* potential;
-} neighours_list;
+     neighbour_list* symteric;
+     neighbour_list* potential;
+} neighours;
+
+typedef struct
+{
+     uint64_t id;
+     uint32_t nonce;
+}  __attribute__((packed, scalar_storage_order("big-endian"))) data_key;
+
+typedef struct data_list
+{
+     data_key key;
+     char* data;
+     neighbour_list* dst;
+} data_list;
+
+extern uint64_t generate_id();
+extern uint32_t generate_nonce();
+extern int check_packet(msg_packet* m);
+
+extern void create_header(msg_packet* m, uint16_t length);
+extern void create_pad1(msg_packet* m);
+extern void create_padN(msg_packet* m, uint8_t length);
+extern void create_short_hello(msg_packet* m);
+extern void create_long_hello(msg_packet* m, uint64_t dst_id);
+extern void create_neighbour(msg_packet* m, peer* p);
+extern void create_data(msg_packet* m, uint8_t length, uint64_t sender_id, uint32_t nonce, uint8_t type, char* msg);
+extern void create_ack(msg_packet* m, uint32_t nonce);
+extern void create_goaway(msg_packet* m, uint8_t length, uint8_t code, char* msg);
+extern void create_warning(msg_packet* m, uint8_t length, char* msg);
 
 
-
-uint64_t init_peer();
-int check_header(msg_hd header);
-int msg_type(char* buffer);
+#endif
